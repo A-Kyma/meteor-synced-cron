f8879080fc42b196ac21b0a2b91980a882e9bb8c
arnaud <adoyen@perso.be>
2024-08-17 22:01:59 +0200

    Update for compatibility of Meteor 3.0 - published file

--- package.js
+++ package.js
@@ -1,14 +1,14 @@
 Package.describe({
   summary: "Define and run scheduled jobs across multiple servers.",
-  version: "1.5.2",
-  name: "percolate:synced-cron",
+  version: "3.0.1",
+  name: "akyma:synced-cron",
   git: "https://github.com/percolatestudio/meteor-synced-cron.git"
 });
 
-Npm.depends({later: "1.1.6"});
+Npm.depends({later: "1.2.0"});
 
 Package.onUse(function (api) {
-  api.versionsFrom('METEOR@1.3');
+  api.versionsFrom(['2.16','3.0']);
   api.use(['underscore', 'check', 'mongo', 'logging'], 'server');
   api.addFiles(['synced-cron-server.js'], "server");
   api.export('SyncedCron', 'server');
