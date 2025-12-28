0e1a479b3e22bb34845b58c53e0087dc9caadd58
Arnaud Doyen <arnaud.doyen@a-kyma.com>
2025-12-28 15:03:48 +0100

    Remove _ensureIndex and replace it by createIndex for v3 migration

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   summary: "Define and run scheduled jobs across multiple servers.",
-  version: "3.0.1",
+  version: "3.0.2",
   name: "akyma:synced-cron",
   git: "https://github.com/percolatestudio/meteor-synced-cron.git"
 });
